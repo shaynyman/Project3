04072cd81c4045f9a384bc096480859bc8b07617
shaynyman <shay.nyman@southeasttech.edu>
2026-05-08 06:42:37 -0700

    Added function/button to add new list items. State updates when user adds item.

---
+++ my-react-app/AddItems.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function AddItems({ setItems }) {           //Function to update list of items
+  const [input, setInput] = useState("");
+
+  function handleSubmit(e) {                //Runs when form is submitted
+    e.preventDefault();                     //Prevents browser from refreshing
+    if (!input.trim()) return;              //Safeguards against adding empty strings
+
+    setItems(prev => [...prev, input.trim()]);      //Creates a new array with previous list items and adds new item to end of list.
+    setInput("");                                   //Clears text box after new item is added
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        value={input}
+        onChange={(e) => setInput(e.target.value)}      //Updates the state when something is typed
+        placeholder="Add an item..."
+      />
+      <button type="submit">Add</button>                //Button to submit form and add new item
+    </form>
+  );
+}
+
+export default AddItems;
